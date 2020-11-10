fc6f5fc4ce2811f4d3605e24644ab27c592e1c6f
Kenya Bogans <kbogans@hotmail.com>
2020-11-10 16:09:54 -0600

    completed AbstractClassesDemoExercise

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,33 +16,41 @@ namespace ConsoleUI
 
             #region Vehicles
 
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
-             */
+
 
             /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
+             * done-Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
+             * done-Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+             * done-Provide the implementations for the abstract methods
+             * done-Only in the Motorcycle class will you override the virtual drive method
             */
 
             // Create a list of Vehicle called vehicles
-
+            var vehicles = new List<Vehicle>();
             /*
-             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
+             *done- Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
-             * Set the properties with object initializer syntax
+             * done-Set the properties with object initializer syntax
              */
-
+            var nissan = new Car() { HasTrunk = true, Make = "Nissan", Model = "Pathfinder", Year = 2017 };
+            var motorcycle = new Motorcycle() { HasSideCart = true, Make = "Yamaha", Model = "Scooter", Year = 2019 };
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Honda", Model = "Accord", Year = 2020 };
+            Vehicle compact = new Car() { HasTrunk = true, Make = "Nissan", Model = "Sentra", Year = 2018 };
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicles.Add(nissan);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(compact);
+
+            foreach (var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveAbstract();
+                Console.WriteLine("------------------------------");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
 
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+             * done-Create an abstract class called Vehicle
+             * done-The vehicle class shall have three string properties Year, Make, and Model
+             * done-Set the defaults to something generic in the Vehicle class
+             * done-Vehicle shall have an abstract method called DriveAbstract with no implementation
+             * done-Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+             */
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive");
+        }
+        
+        
+    }
+}
+
